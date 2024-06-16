2e80753ff281b92175978d77a880186661e895d2
shehroz9712 <shehrysoftologics@gmail.com>
2024-06-16 17:47:35 +0500

    csrf token

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -46,6 +46,7 @@
                 <div class="col-md-6">
                     <div class="wow fadeInUp" data-wow-delay="0.2s">
                         <form method="POST" action="{{ route('user.contact.submit') }}">
+                        @csrf
                             <div class="row g-3">
                                 <div class="col-md-6">
                                     <div class="form-floating">
