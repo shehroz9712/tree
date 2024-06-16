3f6d0829f670b24b406d601fcf39bfa7ec960760
shehroz9712 <shehrysoftologics@gmail.com>
2024-06-16 17:58:47 +0500

    no index tag

--- resources/views/user/layouts/partials/head.blade.php
+++ resources/views/user/layouts/partials/head.blade.php
@@ -4,7 +4,7 @@
     @yield('meta')
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <meta name="robots" content="noindex">
 
     <link href="{{ userFile('img/favicon.ico') }}" rel="icon">
 
